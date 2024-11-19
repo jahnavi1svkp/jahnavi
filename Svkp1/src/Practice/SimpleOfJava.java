433133cb55b3ea13bc2111359753258f78d0301a
LPT-19 <LPT-19@192.168.1.39>
2024-11-19 13:55:03 +0530

    commit1

---
+++ Svkp1/src/Practice/SimpleOfJava.java
@@ -0,0 +1,14 @@
+package Practice;
+
+public class SimpleOfJava {
+	public static void main(String[] args) {
+        int age=19;
+        if(age>18)
+        {
+        	System.out.println("you are eligible");
+        }
+	}
+
+}
+
+	
