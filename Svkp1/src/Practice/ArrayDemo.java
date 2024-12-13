71c8e1c5be593e9495df8379d6fcf9840134624b
LPT-19 <LPT-19@192.168.1.39>
2024-12-13 15:45:13 +0530

    commit3

---
+++ Svkp1/src/Practice/ArrayDemo.java
@@ -0,0 +1,10 @@
+package Practice;
+
+public class ArrayDemo {
+	public static void main(String[] args) {
+		int num[] = {2,3,4};
+		System.out.println(num[0]);
+		System.out.println(num[2]);
+
+}
+}
---
+++ Svkp1/src/Practice/DoWhileDemo.java
@@ -0,0 +1,13 @@
+package Practice;
+
+public class DoWhileDemo {
+
+		public static void main(String[] args) {
+			int i=11;
+			do {
+				System.out.println(i);
+				i++;
+			}while(i<=10);
+			
+}
+}
