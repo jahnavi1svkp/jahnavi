09016e44532a7186e06e08f127091386909686ed
LPT-19 <LPT-19@192.168.1.33>
2024-12-19 14:31:13 +0530

    commit2

---
+++ Svkp1/src/finaldemo/Finalclass.java
@@ -0,0 +1,27 @@
+package finaldemo;
+ class calc 
+	{
+		public void show()
+		{
+			System.out.println("AI integration done by hema");
+		}
+	}
+
+	class adav extends  calc 
+	{
+		public void show()
+		{
+			System.out.println("AI integration done by yasar");
+		}
+	}
+
+
+
+	public class Finalclass {
+
+		public static void main(String[] args) {
+			
+
+		}
+
+	}
